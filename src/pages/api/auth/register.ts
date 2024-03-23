ccb561fd483d7ef2501a4863eb55d73fe54ee78a
Oluwasegun Lawrence <lawrencesegun025@gmail.com>
2024-03-23 22:13:55 +0100

    middleware

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
@@ -7,7 +7,12 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const password = formData.get("password")?.toString();
 
   if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+    return new Response(
+      JSON.stringify({
+        error: "Email and password are required",
+      }),
+      { status: 400 }
+    );
   }
 
   const { error } = await supabase.auth.signUp({
@@ -15,7 +20,12 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     password,
   });
   if (error) {
-    return new Response(error.message, { status: 500 });
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      { status: 500 }
+    );
   }
 
   return redirect("/signin");
--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, redirect, cookies }) => {
   const formData = await request.formData();
@@ -7,7 +7,12 @@ export const POST: APIRoute = async ({ request, redirect, cookies }) => {
   const password = formData.get("password")?.toString();
 
   if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+    return new Response(
+      JSON.stringify({
+        error: "Email and password are required",
+      }),
+      { status: 400 }
+    );
   }
 
   const { data, error } = await supabase.auth.signInWithPassword({
@@ -16,7 +21,12 @@ export const POST: APIRoute = async ({ request, redirect, cookies }) => {
   });
 
   if (error) {
-    return new Response(error.message, { status: 500 });
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      { status: 500 }
+    );
   }
 
   const { access_token, refresh_token } = data.session;
